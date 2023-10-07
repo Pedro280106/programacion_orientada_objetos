a6442ff04e1034cc6d0c59cb82e373ff097b16fa
Pedro280106 <pedroangeldelgado24@gmail.com>
2023-10-07 14:45:31 -0600

    clase 9-30-23

--- proyectopoo/src/com/poo/App.java
+++ proyectopoo/src/com/poo/App.java
@@ -1,12 +1,37 @@
 package com.poo;
 
-import com.poo.model.Persona;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.poo.model.Estudiante;
+import com.poo.model.Materia;
+import com.poo.service.IEstudianteService;
+import com.poo.service.impl.EstudianteServiceImpl;
 
 public class App {
     public static void main(String[] args) throws Exception {
-       Persona persona = new Persona(1, "Pedro", "Masculino", "1976-04-19", false);
-       System.out.println(persona);
+
+        IEstudianteService iEstudianteService = new EstudianteServiceImpl();
+       
+       /*  Persona persona = new Persona("Pedro", "Masculino", "2006-01-28");
+       System.out.println(persona); */
+
+       Estudiante estudiante = new Estudiante("Pedro", "Masculino", "2006-01-28", "ceduk", "Ingeneria en sistemas", 4, 9569);
+
+       List<Materia> materias = new ArrayList<Materia>();
+
+       Materia matematicas = new Materia("Matematicas", 9.0);
+       Materia espanol = new Materia("Español", 8.0);
+       Materia ingles = new Materia("Ingles", 7.7);
+       
+       materias.add(matematicas);
+       materias.add(espanol);
+       materias.add(ingles);
+
+       estudiante.setPromedio(iEstudianteService.promedioFinal(materias));
+       
+       System.out.println(estudiante);
+       
     }
-    
     
 }
--- proyectopoo/src/com/poo/model/Persona.java
+++ proyectopoo/src/com/poo/model/Persona.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeFormatter;
 
 public class Persona {
     // Atributos
-    private Integer id;
     private String nombre;
     private Integer edad;
     private String sexo;
@@ -19,20 +18,18 @@ public class Persona {
 
    /**
     * Constructor de persona
-    * @param id de la persona
     * @param nombre de la persona
     * @param edad de la persona
     * @param sexo de la persona
     * @param fechaNacimiento de la persona
     * @param esAdulto se obtiene en base a la fecha de nacimiento
     */
-    public Persona(Integer id, String nombre, String sexo, String fechaNacimiento, boolean esAdulto) {
-        this.id = id;
+    public Persona(String nombre, String sexo, String fechaNacimiento) {
         this.nombre = nombre;
         this.edad = calcularEdad(fechaNacimiento);
         this.sexo = sexo;
         this.fechaNacimiento = LocalDate.parse(fechaNacimiento, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        this.esAdulto = esAdulto;
+        this.esAdulto = isAdulto(this.edad);
     }
 
     // Getters & Setters
@@ -43,14 +40,6 @@ public class Persona {
 
     public void setNombre(String nombre){
         this.nombre = nombre;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public Integer getEdad() {
@@ -81,44 +70,17 @@ public class Persona {
         this.esAdulto = esAdulto;
     }
 
-    // HashCode & Equals
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Persona other = (Persona) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
     // ToString
 
     @Override
     public String toString() {
-        return "Persona [id=" + id + ", nombre=" + nombre + ", edad=" + edad + ", sexo=" + sexo + ", fechaNacimiento="
+        return "Persona [nombre=" + nombre + ", edad=" + edad + ", sexo=" + sexo + ", fechaNacimiento="
                 + fechaNacimiento + ", esAdulto=" + esAdulto + "]";
     }
 
     // Metodos
 
-    public int calcularEdad(String fechaNac){
+    private int calcularEdad(String fechaNac){
         LocalDate fechaActual = LocalDate.now();
         LocalDate fechaNacLocal = LocalDate.parse(fechaNac, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         int edad = fechaActual.getYear() - fechaNacLocal.getYear();
@@ -132,4 +94,11 @@ public class Persona {
         return edad;
     }
 
+    private boolean isAdulto(int edad){
+        boolean adulto = false;
+        if(edad >= 18){
+            adulto = true;
+        }
+        return adulto;
+    }
 }
